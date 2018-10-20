935d1ce2f917df77479d29534d38c9f9cc592122
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-20 13:57:53 -0700

    saving state updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,29 +26,31 @@ export default class Checkout extends Component<Props> {
   }
 
   public state = {
-    address1: '',
-    address2: '',
-    city: '',
-    country: '',
-    email: '',
-    firstName: '',
-    lastName: '',
-    province: '',
-    zip: '',
+    customer: {
+      address1: '',
+      address2: '',
+      city: '',
+      country: '',
+      email: '',
+      firstName: '',
+      lastName: '',
+      province: '',
+      zip: '',
+    },
   }
 
   public render() {
     return <Query query={query}>
       {({data}) => {
         return <div className='grid-x'>
-          <div className='cell small-12 medium-8'>
+          <div className='cell small-12 medium-7'>
             <h1>{this.props.shopName}</h1>
 
             <fieldset>
               <legend>Customer Information</legend>
               <label>
                 Email
-                <input type='email' value={this.state.email} />
+                <input type='email' value={this.state.customer.email} onChange={this.handleCustomerStateChange('email')} />
               </label>
             </fieldset>
 
@@ -56,49 +58,49 @@ export default class Checkout extends Component<Props> {
               <legend>Shipping Address</legend>
               <label>
                 First Name
-                <input type='text' value={this.state.firstName} />
+                <input type='text' value={this.state.customer.firstName} onChange={this.handleCustomerStateChange('firstName')} />
               </label>
               <label>
                 Last Name
-                <input type='text' value={this.state.lastName} />
+                <input type='text' value={this.state.customer.lastName} onChange={this.handleCustomerStateChange('lastName')} />
               </label>
               <label>
                 Address
-                <input type='text' value={this.state.address1} />
+                <input type='text' value={this.state.customer.address1} onChange={this.handleCustomerStateChange('address1')} />
               </label>
               <label>
                 Apt, Suite, etc.
-                <input type='text' value={this.state.address2} />
+                <input type='text' value={this.state.customer.address2} onChange={this.handleCustomerStateChange('address2')} />
               </label>
               <label>
                 City
-                <input type='text' value={this.state.city} />
+                <input type='text' value={this.state.customer.city} onChange={this.handleCustomerStateChange('city')} />
               </label>
               <label>
                 Country
-                <input type='text' value={this.state.country} />
+                <input type='text' value={this.state.customer.country} onChange={this.handleCustomerStateChange('country')} />
               </label>
               <label>
                 State/Province
-                <input type='text' value={this.state.province} />
+                <input type='text' value={this.state.customer.province} onChange={this.handleCustomerStateChange('province')} />
               </label>
               <label>
                 Zip code
-                <input type='number' value={this.state.zip} />
+                <input type='number' value={this.state.customer.zip} onChange={this.handleCustomerStateChange('zip')} />
               </label>
             </fieldset>
 
             <div className='grid-x'>
               <div className='cell small-6'>
-                <a href='#'>Return to cart</a>
+                <a className='button' href='#'>Return to cart</a>
               </div>
               <div className='cell small-6 text-right'>
-                next
+                <a className='button' href='#'>Next</a>
               </div>
             </div>
           </div>
 
-          <div className='cell show-for-medium medium-4'>
+          <div className='cell show-for-medium medium-5'>
             <h2>Order Summary</h2>
             <div>
               {this.props.cart.items.map(item => (
@@ -109,5 +111,11 @@ export default class Checkout extends Component<Props> {
         </div>
       }}
     </Query>
+  }
+
+  private handleCustomerStateChange = (key: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    const state = this.state
+    state.customer[key] = e.target.value
+    this.setState(state)
   }
 }
