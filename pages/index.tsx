ca4fb7c5ef88aeedaba352d33ca635de89e23ec1
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-20 09:02:22 -0700

    setting cart as a propr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,25 @@
+import {Component} from 'react'
+
 import '../styles/index.scss'
 
-export default () => <div>
-  <h1>Checkout</h1>
-</div>
+interface Props {
+  cart: object,
+}
+
+export default class Checkout extends Component<Props> {
+  public static getInitialProps(ctx): Props {
+    const cart = JSON.parse(decodeURIComponent(ctx.req.session.cart))
+    return {cart}
+  }
+
+  public render() {
+    return <div className='grid-x'>
+      <div className='cell small-12 medium-8'>
+        <h1>Checkout</h1>
+      </div>
+      <div className='cell show-for-medium medium-4'>
+        right
+      </div>
+    </div>
+  }
+}
--- server/init-session.ts
+++ server/init-session.ts
@@ -2,9 +2,12 @@ import * as Koa from 'koa'
 
 export default () => async (ctx: Koa.Context, next: () => any) => {
   if (ctx.method === 'POST') {
+    // for POST requests, we save the POST body to the session for later use
     ctx.session = ctx.request.body as any
     ctx.redirect('/')
   } else {
+    // for other requests, we set the session to the req object so next.js can access it
+    (ctx.req as any).session = ctx.session
     await next()
   }
 }
